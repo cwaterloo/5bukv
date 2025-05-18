480e7af4e7e78bcfb7ee4d9eecd8711b58ad4434
Christoph Waterloo <cwaterloo@users.noreply.github.com>
2025-05-18 10:47:59 -0700

    Specify read access for loading and write access for savin.

--- 5LettersLib/TreeSerializer.cs
+++ 5LettersLib/TreeSerializer.cs
@@ -8,14 +8,14 @@ namespace FiveLetters
     {
         public static void Save(Tree tree, string outputFilename)
         {
-            using FileStream fileStream = new(outputFilename, FileMode.Create);
+            using FileStream fileStream = new(outputFilename, FileMode.Create, FileAccess.Write, FileShare.None);
             using GZipStream gZipStream = new(fileStream, CompressionLevel.SmallestSize);
             using CodedOutputStream codedOutputStream = new(gZipStream);
             tree.WriteTo(codedOutputStream);
         }
 
         public static Tree Load(string inputFileName) {
-            using FileStream fileStream = new(inputFileName, FileMode.Open);
+            using FileStream fileStream = new(inputFileName, FileMode.Open, FileAccess.Read, FileShare.Read);
             using GZipStream gZipStream = new(fileStream, CompressionMode.Decompress);
             using CodedInputStream codedInputStream = new(gZipStream);
             return Tree.Parser.ParseFrom(codedInputStream);
