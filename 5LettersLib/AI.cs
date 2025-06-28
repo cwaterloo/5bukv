98327eab5912fc2590a714f33178cde4e38421c3
Christoph Waterloo <cwaterloo@users.noreply.github.com>
2025-06-27 20:50:44 -0700

    Reapply "Allow to check 2 words at a time."
    This reverts commit 6697c142a1dbc5ccc77bc743c33040c42988d485.

--- 5LettersLib/AI.cs
+++ 5LettersLib/AI.cs
@@ -2,6 +2,27 @@ namespace FiveLetters
 {
     public static class AI
     {
+        private static long GetMatchWordCount(List<Word> words, Word word, Word guess1, Word guess2, long current, long observedMin)
+        {
+            long metric = current;
+            long n = 0;
+            State state1 = new(word, guess1);
+            State state2 = new(word, guess2);
+            foreach (Word wordToCheck in words)
+            {
+                if (state1.MatchWord(wordToCheck) && state2.MatchWord(wordToCheck))
+                {
+                    metric += (n << 1) + 1;
+                    ++n;
+                    if (metric > observedMin)
+                    {
+                        return metric;
+                    }
+                }
+            }
+            return metric;
+        }
+
         public static long GetMatchWordCount(List<Word> words, Word word, Word guess, long current, long observedMin)
         {
             long p = words.Count / 243;
@@ -47,6 +68,44 @@ namespace FiveLetters
                 {
                     candidateMin = globalWords[i];
                     minMetric = currentMetric;
+                }
+            }
+
+            if (candidateMin.HasValue)
+            {
+                return candidateMin.Value;
+            }
+
+            throw new InvalidOperationException("No more words left.");
+        }
+
+        public static (Word guess1, Word guess2) GetCandidate2(List<Word> words, List<Word> globalWords)
+        {
+            if (words.Count < 2)
+            {
+                throw new InvalidOperationException("Expect at least 2 candidates.");
+            }
+
+            long minMetric = long.MaxValue;
+            (Word candidate1, Word candidate2)? candidateMin = null;
+            for (int i = 0; i < globalWords.Count; ++i)
+            {
+                for (int j = i + 1; j < globalWords.Count; ++j)
+                {
+                    long currentMetric = 0;
+                    foreach (Word word in words)
+                    {
+                        currentMetric = GetMatchWordCount(words, word, globalWords[i], globalWords[j], currentMetric, minMetric);
+                        if (currentMetric > minMetric)
+                        {
+                            break;
+                        }
+                    }
+                    if (currentMetric < minMetric)
+                    {
+                        candidateMin = (globalWords[i], globalWords[j]);
+                        minMetric = currentMetric;
+                    }
                 }
             }
 
